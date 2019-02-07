a9d7cd2a6f7f91163d3d608d6d200144c2181d6a
Mahmoud Ahmed <mahmoudahmed@Mahmouds-MacBook-Air.local>
2019-02-07 14:38:06 +0900

    minor changes to pass BioChecks

---
+++ NEWS.md
@@ -0,0 +1,7 @@
+# curatedAdipoRNA 0.99.1
+
+* Minor changes for pass BiocCheck
+
+# curatedAdipoRNA 0.99.0
+
+* Submitted to Bioconductor
--- R/data.R
+++ R/data.R
@@ -1,10 +1,10 @@
 #' Gene counts of differentiating adipocytes
 #'
-#' @format A \code{\link[SummarizedExperiment]{SummarizedExperiment}} object
+#' @format A \code{RangedSummarizedExperiment} object
 #' contains:
 #'
 #' \describe{
-#' \item{gene_counts}{The gene counts \code{matrix}.}
+#' \item{assay}{The gene counts \code{matrix}.}
 #' \item{colData}{The phenotype data.}
 #' \item{rowRanges}{The feature data}
 #' \item{metadata}{The metadata \code{metadata} which contain two objects;
@@ -12,4 +12,14 @@
 #' each \code{fastq} file and a \code{data.frame} of the studies from which
 #' the samples were collected.}
 #' }
+#'
+#' @examples
+#' # load the package
+#' library(curatedAdipoRNA)
+#'
+#' # load the data object
+#' data('gene_counts')
+#'
+#' # print the object
+#' gene_counts
 "gene_counts"
